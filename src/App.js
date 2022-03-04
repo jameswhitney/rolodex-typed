e03184863a787961e39c16a7761e6a9befb34ac6
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-04 12:27:55 -0500

    Creates state object and fetch call to populate list of h1 elements

--- src/App.js
+++ src/App.js
@@ -2,10 +2,40 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  // Per React docs setState using a function that returns the new state object
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((resp) => resp.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Changes App to class based component</h1>
+        {
+          // Map over monsters array to create list of h1 elements
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
