3ffa9012efbe30667efa5914bd7e7b3ed6ba0f1f
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-04 13:23:02 -0500

    searchField state value used to keep track of input so filtering works properly

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchString: "",
     };
+    // Logging used to show order in which lifecycles are run
+    console.log("constructor");
   }
 
   // Per React docs setState using a function that returns the new state object
   componentDidMount() {
+    // Logging used to show order in which lifecycles are run
+    console.log("componentDidMount");
     fetch(`https://jsonplaceholder.typicode.com/users`)
       .then((resp) => resp.json())
       .then((users) =>
@@ -18,17 +23,38 @@ class App extends Component {
           () => {
             return { monsters: users };
           },
+          // Logging current state instead of previous state
           () => console.log(this.state)
         )
       );
   }
 
   render() {
+    // Logging used to show order in which lifecycles are run
+    console.log("render");
+
+    const filterMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchString);
+    });
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          // This example of using an anonymous function is a bad practice
+          // Once it is run it is no longer in memory. So for each re-render
+          // the function must be created again and again, etc
+          onChange={(event) => {
+            const searchString = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchString };
+            });
+          }}
+        />
         {
           // Map over monsters array to create list of h1 elements
-          this.state.monsters.map((monster) => {
+          filterMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
