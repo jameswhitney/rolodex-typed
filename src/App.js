f3745a218491af4515f453af2be1b2ef746918c0
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-05 15:32:57 -0500

    Begin refator of App to functional component. SearchBox converted

--- src/App.js
+++ src/App.js
@@ -1,58 +1,80 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  console.log(searchField);
 
-  // Per React docs setState using a function that returns the new state object
-  componentDidMount() {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((resp) => resp.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
-
-  // Better practice than using anonymous function used in render
-  // More functions used in render will affect performance
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
 
-  render() {
-    // Destructure state variables and class methods
-    // to make code more readable imo
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="monsters-searchbox"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-searchbox"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      {/* 
+    <CardList monsters={filteredMonsters} /> */}
+    </div>
+  );
+};
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   // Per React docs setState using a function that returns the new state object
+//   componentDidMount() {
+//     fetch(`https://jsonplaceholder.typicode.com/users`)
+//       .then((resp) => resp.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { monsters: users };
+//         })
+//       );
+//   }
+
+//   // Better practice than using anonymous function used in render
+//   // More functions used in render will affect performance
+// onSearchChange = (event) => {
+//   const searchField = event.target.value.toLocaleLowerCase();
+//   this.setState(() => {
+//     return { searchField };
+//   });
+// };
+
+//   render() {
+//     // Destructure state variables and class methods
+//     // to make code more readable imo
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+//     return (
+//       <div>
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//           className="monsters-searchbox"
+//           onChangeHandler={onSearchChange}
+//           placeholder="search monsters"
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
