6bcd6c5edae656e484753ca38cbdf04b20150732
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-05 16:30:08 -0500

    App.js refactored as functional component. See comments for implementaion details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 const App = () => {
-  const [searchField, setSearchField] = useState(""); // [value, setValue]
-  console.log(searchField);
+  // When props change or state changes
+  // Entire functional component is run
+  // again which re-renders UI
+
+  // setValue does not trigger a re-render
+  // unless stateValue changes then React will
+  // re-render/run the function again.
+  const [searchField, setSearchField] = useState(""); // [stateValue, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // useEffect takes two arguments
+  // first is callback, second is an array
+  // of dependencies like props.
+  // useEffect runs very first time App mounts.
+  // If dependencies change, useEffect is called again
+  // useEffect will not run on subsequent re-renders
+  // unless array of dependencies changes
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((resp) => resp.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  // Moved filter functionalitiy into useEffect
+  // Added monsters state and searchField state
+  // to dependencies array so we only filter
+  // when either monsters array changes(it shouldn't in this app but it could)
+  // or searchField string changes
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
@@ -21,60 +54,9 @@ const App = () => {
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
       />
-      {/* 
-    <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-
-//   // Per React docs setState using a function that returns the new state object
-//   componentDidMount() {
-//     fetch(`https://jsonplaceholder.typicode.com/users`)
-//       .then((resp) => resp.json())
-//       .then((users) =>
-//         this.setState(() => {
-//           return { monsters: users };
-//         })
-//       );
-//   }
-
-//   // Better practice than using anonymous function used in render
-//   // More functions used in render will affect performance
-// onSearchChange = (event) => {
-//   const searchField = event.target.value.toLocaleLowerCase();
-//   this.setState(() => {
-//     return { searchField };
-//   });
-// };
-
-//   render() {
-//     // Destructure state variables and class methods
-//     // to make code more readable imo
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-//     return (
-//       <div>
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           className="monsters-searchbox"
-//           onChangeHandler={onSearchChange}
-//           placeholder="search monsters"
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
