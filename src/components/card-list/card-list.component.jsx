4aa343b1b4a847cdcb36a47e6b213c8b3fe444c7
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-08 11:15:06 -0500

    Refactor: All components are functional components instead of class based

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,15 @@
-import { Component } from "react";
-
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          // Destructure properties from monster
-          // in case future iterations use different
-          // key names
-          const { id } = monster;
-          return <Card key={id} monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      // Destructure properties from monster
+      // in case future iterations use different
+      // key names
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,24 +1,20 @@
-import { Component } from "react";
-
 import "./card.styles.css";
 
 // This component handles the generic
 // card logic for re-use in other parts of
 // application. Currently used in CardList
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster;
-    return (
-      <div className="card-container">
-        <img
-          alt={`monster ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container">
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
